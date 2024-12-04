440bc221117c53c918e6265b0003606d3334306c
hipii <durgamagar1098@gmail.com>
2024-12-04 20:43:10 +0545

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,27 @@ function App() {
     <>
       <div className='maindiv'>
         <div className='leftside'>
-          <div className='icons'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g fill="none"><path fill="#ca0b4a" d="M29.2 2.95c-.947-.947-2.7-.998-3.818-.425c-.913.314-1.874.667-2.854 1.063l.01.01c-2.575 1.095-6.427 3.04-9.51 5.902c-2.164 2.01-3.404 3.556-4.235 4.965l-5.298 1.95a1 1 0 0 0-.362 1.646l11.138 11.137a1 1 0 0 0 1.645-.362l2.204-5.986c1.608-.958 3.223-2.165 4.908-3.85c2.39-2.39 4.366-6.56 5.513-9.357l.009.008c.4-.99.758-1.961 1.074-2.883c.573-1.118.74-2.654-.424-3.818" /><path fill="#f4f4f4" d="M23.299 4.365c-2.517 1.04-6.478 2.978-9.59 5.868c-2.015 1.87-3.172 3.294-3.94 4.55c-.769 1.255-1.175 2.38-1.663 3.74l-.002.0  07c-.17.474-.35.977-.559 1.514l4.556 4.556c3.602-1.48 6.74-2.828 10.22-6.307c2.351-2.352 4.339-6.642 5.454-9.421z" /><path fill="#9b9b9b" d="M24.528 11.25a3.25 3.25 0 1 1-6.5 0a3.25 3.25 0 0 1 6.5 0" /><path fill="#83cbff" d="M23.528 11.25a2.25 2.25 0 1 1-4.5 0a2.25 2.25 0 0 1 4.5 0" /><path fill="#ff8257" d="M2.451 29.61C1.744 28.905 2.028 24 4.528 23c0 0 2.5-1 4.11.6c1.612 1.601.89 3.4.89 3.4c-.707 2.121-3.718 2.965-4.071 2.61c-.195-.194.156-.55 0-.706c-.157-.157-.398.022-1.06.353c-.472.236-1.663.637-1.946.354" /><path fill="#533566" d="M6.088 21.06a1.5 1.5 0 0 1 2.122 0l3.535 3.536a1.5 1.5 0 1 1-2.121 2.122l-3.536-3.536a1.5 1.5 0 0 1 0-2.121" /><path fill="#f92f60" d="M15.535 18.722c.442-1.2-.725-2.368-1.926-1.926l-7.114 2.619a1 1 0 0 0-.362 1.646l5.138 5.137a1 1 0 0 0 1.645-.362z" /></g></svg>
-          </div>
-          <div className='description'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In eum quia dolorum recusandae porro reprehenderit reiciendis velit iusto nesciunt placeat eos commodi enim itaque ducimus, nisi animi suscipit, maiores iure.
-          </div>
-          <button className='button'>Readmore</button>
-        </div>
+            <div className='icons'><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128"><path fill="#ffb803" d="M99.32 28.84L93.51 9.5l-1.98-2.66s-.15-.18-.17-.24c-.06-.25-.13-.66.11-.97c.18-.23.49-.29.85-.26c.14.01.48.14 1.02.44c5.31 3 28.21 21.75 30.14 52.02c1.09 17.1-5.15 35.4-18.27 48.16c-12.46 12.11-31.28 19.65-51.98 16.7c-30.94-4.41-44.22-23.21-47.69-29.42c-.61-1.1-.91-1.8-.97-1.97c-.82-2.15-.6-3.24.17-3.49c.2-.06.65.02.9.3c1 1.12 2.82 1.85 2.82 1.85l12.88 10.43l63.89 4.84l22.65-49.38z" /><path fill="#ffca29" d="M73.65 87.67c15.97-9.9 23.77-26.72 24.39-42.28c.9-22.79-6.68-38.8-6.68-38.8s14.01 11.5 21.68 28.08s9 43.87-7.78 63.4c-18.45 21.45-43.18 22.72-58.73 18.7c-32.27-8.35-41.09-28.83-41.09-28.83s17.78 9.95 37.77 8.9c14.88-.78 22.54-4.27 30.44-9.17" /></svg>
+            </div>
+            <div className='description'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In eum quia dolorum recusandae porro reprehenderit reiciendis velit iusto nesciunt placeat eos commodi enim itaque ducimus, nisi animi suscipit, maiores iure.
+            </div>
 
-        
-        <div className='rightside'>
-          <div className='Signup'>Sign Up</div>
-          <div className='input1'>
-            <input type='text' placeholder='username' />
-            <input type='password' placeholder='Enter a password' />
+            <button className='button'>Readmore</button>
           </div>
-        <button className='submit'>Submit</button>
-      </div>
-      </div>
-    </>
-  )
+
+
+          <div className='rightside'>
+            <div className='Signup'>Sign Up</div>
+            <div className='input1'>
+              <input type='text' placeholder='Username' />
+              <input type='password' placeholder='Enter a password' />
+            </div>
+            <button className='submit'>Submit</button>
+          </div>
+        </div>
+      </>
+      )
 }
 
-export default App
+      export default App
